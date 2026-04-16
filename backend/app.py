798f07a1b806d2372c542a830d1b2747140adfd7
Aswath Pranav Balan T <ashd2607@gmail.com>
2026-04-16 12:59:12 +0530

    Analyze and Extension Update1

--- backend/app.py
+++ backend/app.py
@@ -427,7 +427,7 @@ PRIVATE_TYPES = {
 }
 
 provider   = NlpEngineProvider(nlp_configuration={
-    'nlp_engine_name':'spacy','models':[{'lang_code':'en','model_name':'en_core_web_lg'}]
+    'nlp_engine_name':'spacy','models':[{'lang_code':'en','model_name':'en_core_web_sm'}]
 })
 nlp_engine = provider.create_engine()
 analyzer   = AnalyzerEngine(nlp_engine=nlp_engine, supported_languages=["en"])
